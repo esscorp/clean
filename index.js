aa7c37a41746543c939fe572a53a0180e6165b95
AndyMori <andy.mori@hotmail.com>
2019-09-03 16:45:40 -0500

    handles better cases

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ exports.name = function(name) {
 };
 
 exports.isEmpty = function(phrase) {
-	if (phrase === undefined || phrase === null || phrase === '') return true;
+	if (phrase === undefined || phrase === null || phrase === '' || phrase.trim === '') return true;
 	return false;
 };
 
